5a335ee1364f59b2c5904cabc85382e5ee82bb82
plopezrodrigo <patrylr84@gmail.com>
2022-10-20 18:06:47 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function excuse() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.querySelectorAll("#excuse");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const whoRandom = who[Math.round(Math.random() * (who.length - 1))];
+  const whatRandom = what[Math.round(Math.random() * (what.length - 1))];
+  const whenRandom = when[Math.round(Math.random() * (when.length - 1))];
+  document.getElementById("excuse").innerHTML =
+    "<div>My " +
+    whoRandom +
+    " " +
+    whatRandom +
+    "<br> my " +
+    whenRandom +
+    ".</div>";
 };
